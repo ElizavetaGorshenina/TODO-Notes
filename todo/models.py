11864e36c719cf55a2f7d707b1891f0b67473d0e
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-10 22:51:41 +0300

    lesson_3 Models Project and ToDo added

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from uuid import uuid4
+from users.models import User
 
-# Create your models here.
+
+class Project(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    name = models.CharField(max_length=32)
+    user = models.ManyToManyField(User)
+    link_to_repo = models.CharField(max_length=256, unique=True)
+
+
+class ToDo(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True)
+    project = models.ForeignKey(Project, models.PROTECT)
+    text = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, models.PROTECT)
+    active = models.BooleanField(default=False)
