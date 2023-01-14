3c5df804001fb9b1f867cec7273c1add98b00a14
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-14 22:40:29 +0300

    lesson_4 Project API edited

--- todo/views.py
+++ todo/views.py
@@ -1,11 +1,19 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import LimitOffsetPagination
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
+from .filters import ProjectFilter
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class ToDoModelViewSet(ModelViewSet):
