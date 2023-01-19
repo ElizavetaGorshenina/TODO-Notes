6e150a069754cba5505d91c1bb9f107a744492b8
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-19 10:52:15 +0300

    lesson_5 Data loading from backend added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-//import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
 import MenuItem from './components/Menu.js'
 import UserList from './components/User.js'
@@ -8,48 +8,42 @@ import UserProjectList from './components/UserProject.js'
 import ToDoList from './components/ToDo.js'
 import NotFound404 from './components/NotFoundPage'
 import FooterItem from './components/Footer.js'
-//import axios from 'axios'
-import {HashRouter, Route, Switch, Redirect} from 'react-router-dom'
+import axios from 'axios'
+import {Route, Switch, Redirect, BrowserRouter} from 'react-router-dom'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    const user1 = {username: "Max", firstname: "Maxim", lastname: "Aleksandrov", email: "max_1980@gmail.com"}
-    const user2 = {username: "Innes", firstname: "Inna", lastname: "Belskaya", email: "innes_bell@mail.ru"}
-    const users = [user1, user2]
-    const project1 = {name: "Group Work", user: [user1, user2], link_to_repo: "group_work_link"}
-    const project2 = {name: "Home Study", user: [user2], link_to_repo: "innas_home_study_link"}
-    const projects = [project1, project2]
-    const todo1 = {project: project2, text: "To watch the video (lesson1)", created: "2023-01-10T20:39:32.372302Z", updated: "i2023-01-10T20:39:32.375008Z", user: user2, active: 'False'}
-    const todo2 = {project: project2, text: "To watch the video (lesson2)", created: "2023-01-10T20:39:32.372302Z", updated: "i2023-01-10T20:39:32.375008Z", user: user2, active: 'True'}
-    const todoes = [todo1, todo2]
     this.state = {
-      'users': users,
-      'projects': projects,
-      'todoes': todoes
-//      'users': []
-
+      'users': [],
+      'projects': [],
+      'todoes': []
     }
   }
-
-  // componentDidMount() {
-  //   axios.get('http://127.0.0.1:8000/users/users')
-  //     .then(response => {
-  //       const users = response.data
-  //         this.setState(
-  //         {
-  //           'users': users
-  //         }
-  //       )
-  //     }).catch(error => console.log(error))
-  // }
-    
+  
+  componentDidMount() {
+    axios.all([
+      axios.get('http://127.0.0.1:8000/users/'),
+      axios.get('http://127.0.0.1:8000/projects/'),
+      axios.get('http://127.0.0.1:8000/todo/')
+    ]).then(axios.spread((res_users, res_projects, res_todoes) => {
+      const users = res_users.data.results
+      const projects = res_projects.data.results
+      const todoes = res_todoes.data.results
+        this.setState({
+          'users': users,
+          'projects': projects,
+          'todoes': todoes
+        });
+      })
+      ).catch(error => console.log(error))
+  }
 
   render () {
     return (
-      <div>
-        <HashRouter>
+      <div className='App'>
+        <BrowserRouter>
           <MenuItem />
           <Switch>
             <Route exact path='/users' component={() => <UserList users={this.state.users} />} />
@@ -57,10 +51,11 @@ class App extends React.Component {
             <Route exact path='/user/:username' component={() => <UserProjectList projects={this.state.projects} />} />
             <Route exact path='/todoes' component={() => <ToDoList todoes={this.state.todoes} />} />
             <Redirect from='/todos' to='/todoes' />
+            <Redirect from='/' to='/users' />
             <Route component={NotFound404} />
           </Switch>
           <FooterItem />
-        </HashRouter>
+        </BrowserRouter>
       </div>
     )
   }
