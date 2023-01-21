29c60d6f6136357bbf90174354692336235b7db2
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-21 23:21:45 +0300

    lesson_6 JWT Authentication added

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
+#from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 #from users.views import UserModelViewSet
 from users.views import UserCustomViewSet
@@ -31,6 +32,7 @@ router.register('todo', ToDoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('rest_framework.urls')),
-    path('auth-token/', obtain_auth_token),
+    path('auth-jwt/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('auth-jwt/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('', include(router.urls)),
 ]
