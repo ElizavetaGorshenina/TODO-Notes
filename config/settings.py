8f5823873461411caead8d0f148a999c5188d56a
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-21 21:48:51 +0300

    lesson_6 Basic Authentication added

--- config/settings.py
+++ config/settings.py
@@ -138,5 +138,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissions',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
     ]
 }
