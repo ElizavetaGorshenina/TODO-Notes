323821dac47519124d10e2d30332b3e5540c25aa
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-29 00:43:48 +0300

    lesson_8 A test with the use of APIRequestFactory passed

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,18 @@
+import json
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory, force_authenticate
+from .views import ToDoModelViewSet
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class TestToDoViewSet(TestCase):
+
+    def test_get_list_auth(self):
+        factory = APIRequestFactory()
+        user = User.objects.create_user('developer', 'hanna@develops.com', 'hannadevelops')
+        request = factory.get('/todo')
+        force_authenticate(request, user)
+        view = ToDoModelViewSet.as_view({'get':'list'})
+        response = view(request)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
