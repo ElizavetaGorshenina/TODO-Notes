260eacf9e5fa6b43cdb049b60befad95fe386df2
ElizavetaGorshenina <liza_summer@mail.ru>
2023-01-29 00:47:19 +0300

    lesson_8 A test with the use of APIClient passed

--- todo/tests.py
+++ todo/tests.py
@@ -1,8 +1,9 @@
 import json
 from django.test import TestCase
 from rest_framework import status
-from rest_framework.test import APIRequestFactory, force_authenticate
+from rest_framework.test import APIRequestFactory, force_authenticate, APIClient
 from .views import ToDoModelViewSet
+from users.models import User as ToDoUser
 from django.contrib.auth.models import User
 
 
@@ -16,3 +17,12 @@ class TestToDoViewSet(TestCase):
         view = ToDoModelViewSet.as_view({'get':'list'})
         response = view(request)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class TestUserViewSet(TestCase):
+
+    def test_get_detail_guest(self):
+        user = ToDoUser.objects.create(username='Paul', firstname='Paul', lastname='Moses', birth_date='1981-08-11', email='paul@home.com')
+        client = APIClient()
+        response = client.get(f'/users/{user.id}/')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
